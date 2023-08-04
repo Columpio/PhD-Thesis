36224aead37e4cd4f03a9fbd17a5c27c3aabee7f
Yurii Kostyukov <kostyukov.yurii@gmail.com>
2023-08-04 15:16:23 +0300

    chore

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 cd PhDThesis
-make clean
 make dissertation
 cd ../PhDThesisEng
-make clean
 make dissertation
 cd ..
+rm -f dissertation.pdf
 sejda-console merge -b retain -f PhDThesis/dissertation.pdf PhDThesisEng/dissertation.pdf -o dissertation.pdf
