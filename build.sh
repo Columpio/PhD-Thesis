3752e8db8cc3fc7a85b75299d3f75f1cf1f4ff1c
Yurii Kostyukov <kostyukov.yurii@gmail.com>
2023-09-04 11:22:24 +0300

    important fix

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ make dissertation
 cd ../PhDThesisEng
 make clean
 make dissertation
-cd ..
-rm -f dissertation.pdf
-sejda-console merge -b retain -f PhDThesis/dissertation.pdf PhDThesisEng/dissertation.pdf -o dissertation.pdf
+# cd ..
+# rm -f dissertation.pdf
+# sejda-console merge -b retain -f PhDThesis/dissertation.pdf PhDThesisEng/dissertation.pdf -o dissertation.pdf
