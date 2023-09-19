67b6b618aa614f3ceb507a1b7caa684bf0858697
Yurii Kostyukov <kostyukov.yurii@gmail.com>
2023-09-19 13:40:25 +0300

    proof read Evaluation

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 find . -iname "*.pdf" -delete
-cd PhDThesis
-make clean
-make dissertation
-cd ../PhDThesisEng
-make clean
-make dissertation
+(trap 'kill 0' SIGINT; (cd PhDThesis && make clean && make dissertation) & (cd PhDThesisEng && make clean && make dissertation) & wait)
 # cd ..
 # rm -f dissertation.pdf
 # sejda-console merge -b retain -f PhDThesis/dissertation.pdf PhDThesisEng/dissertation.pdf -o dissertation.pdf
