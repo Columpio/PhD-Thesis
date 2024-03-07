d0b4c8a1efc8f9bf2bf552560d251ea371bb1196
Yurii Kostyukov <kostyukov.yurii@gmail.com>
2024-03-07 17:20:13 +0300

    fixes

--- slides/build.sh
+++ slides/build.sh
@@ -1,2 +1,4 @@
 pdflatex --interaction nonstopmode -shell-escape preso.tex
-
+bibtex preso
+pdflatex --interaction nonstopmode -shell-escape preso.tex
+pdflatex --interaction nonstopmode -shell-escape preso.tex
